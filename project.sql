f6325377e8a2f6ab42abcf23c8cad20e326445d6
vaibagga <vaibagga@gmail.com>
2019-03-25 19:48:14 +0530

    relations

--- project.sql
+++ project.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE CourierManagementSystem;
+
 USE CourierManagementSystem;
 
 
@@ -16,58 +17,56 @@ USE CourierManagementSystem;
 --9. AddressLine3: City of Customer
 --10. Email: Email Id of Customer
 
-
---Login Table
---
-CREATE TABLE Login(
-	Login_username VARCHAR(50) , 
-	Admin_id INT(50),
-	PASSWORD VARCHAR(50) 
-);
-
-
-	
-CREATE TABLE ADMIN(
-	NAME VARCHAR(50),
-	Office_Id  INT(50), 
-	Id INT(50),
-	Phone_Number INT(50) 
-);
-	
-CREATE TABLE Courier(
-	Admin_id VARCHAR(50) PRIMARY KEY,
-	Office_id INT(50),dbdbms_labtest
-	Shipment_id INT(50),
-	Title VARCHAR(50),
-	STATUS VARCHAR(50) 
-);
 CREATE TABLE Customers(
-						CustomerId INT NOT NULL PRIMARY KEY,
-						FirstName VARCHAR(20) NOT NULL,
-						LastName VARCHAR(20) NOT NULL,
-						MiddleName VARCHAR(20) DEFAULT NULL,
-						PhoneNo INT NOT NULL,
-						AddressLine1 VARCHAR(30),
-						AddressLine2 VARCHAR(30),
-						AddressLine3 VARCHAR(30),
-						ZipCode INT NOT NULL;
-						Email VARCHAR(30)
+	CustomerId INT NOT NULL PRIMARY KEY,
+	FirstName VARCHAR(20) NOT NULL,
+	LastName VARCHAR(20) NOT NULL,
+	MiddleName VARCHAR(20) DEFAULT NULL,
+	PhoneNo INT NOT NULL,
+	AddressLine1 VARCHAR(30),
+	AddressLine2 VARCHAR(30),
+	AddressLine3 VARCHAR(30),
+	ZipCode INT NOT NULL;
+	Email VARCHAR(30)
 );
-INSERT INTO Customers VALUES(1,"John", "Smith", "K", 2134190142, "House 12", "Privet Drive, Surrey", "London, England", "john@gmail.com");
-INSERT INTO Customers VALUES(2,"Joanne", "Smith", "A", 1234563891, "House 12", "Privet Drive, Surrey", "London, England", "joanne@gmail.com");
-INSERT INTO Customers VALUES(3,"Juan", "Smith", "K", 9234567890, "House 12", "Privet Drive, Surrey", "London, England", "juan@gmail.com");
-INSERT INTO Customers VALUES(4,"Jack", "Smith", "K", 8234567890, "House 12", "Privet Drive, Surrey", "London, England", "jackn@gmail.com");
-INSERT INTO Customers VALUES(5,"John", "Wick", "K", 7234567890, "House 12", "Privet Drive, Surrey", "London, England", "johnw@gmail.com");
-INSERT INTO Customers VALUES(6,"Harry", "Potter", "J", 6934567890, "House 12", "Privet Drive, Surrey", "London, England", "harryp@gmail.com");
-INSERT INTO Customers VALUES(7,"Frank", "Abagnale", "K", 4204567890, "House 12", "Privet Drive, Surrey", "London, England", "frank@gmail.com");
-INSERT INTO Customers VALUES(8,"Gandalf", "Grey", "The", 8904567890, "House 12", "Privet Drive, Surrey", "London, England", "gandalf@gmail.com");
-INSERT INTO Customers VALUES(9,"Aragorn", "Elessar", "", 2224567890, "House 12", "Privet Drive, Surrey", "London, England", "aragorn@gmail.com");
-INSERT INTO Customers VALUES(10,"Frodo", "Baggins", "", 2134567890, "House 12", "Privet Drive, Surrey", "London, England", "frodo@gmail.com");
-INSERT INTO Customers VALUES(11,"Ron", "Weasley", "K", 0114567890, "House 12", "Privet Drive, Surrey", "London, England", "weasley6@gmail.com");
-INSERT INTO Customers VALUES(12,"Fred", "Weasley", "K", 0124567890, "House 12", "Privet Drive, Surrey", "London, England", "fweasley@gmail.com");
-INSERT INTO Customers VALUES(13,"Saruman", "White", "The", 0864567890, "House 12", "Privet Drive, Surrey", "London, England", "saruman@gmail.com");
-INSERT INTO Customers VALUES(14,"Sahil", "Sulekhiya", "K", 0424567890, "House 12", "Privet Drive, Surrey", "London, England", "johns@gmail.com");
-INSERT INTO Customers VALUES(15,"Frank", "Castle", "K", 1934567890, "House 12", "Privet Drive, Surrey", "London, England", "smith@gmail.com");
+
+--Admin Login Table
+--1. AdminUsername:  
+--2. Password: 
+
+
+CREATE TABLE AdminLogin(
+	AdminUsername INT(50) PRIMARY KEY,
+	FOREIGN KEY AdminId REFERENCES Admin
+	Password VARCHAR(50) 
+);
+
+
+--Customer Login Table
+--1. CustomerUsername: Username for customer
+--2. Password: Password for login 
+
+CREATE TABLE CustomerLogin(
+	CustomerUsername VARCHAR(50),
+	FOREIGN KEY CustomerId REFERENCES Customers(CustomerId),
+	Passord VARCHAR(50)
+);
+
+
+--Records of administrators
+--1. Name
+--2. Id
+--3. PhoneNumber
+	
+CREATE TABLE Admin(
+	FOREIGN KEY LoginUsername REFERENCES Login(AdminUsername),
+	NAME VARCHAR(50),
+	OfficeId  INT(50), 
+	Id INT(50),
+	PhoneNumber INT(50) 
+);
+	
+
 
 SELECT * FROM Customers;
 
@@ -85,24 +84,6 @@ SELECT * FROM Customers;
 --10. Email: Email Id of Customer
 
 
-
-
-CREATE TABLE Office(
-	OfficeId INT NOT NULL PRIMARY KEY,
-
-);
-
-
-CREATE TABLE Employees(
-	EmployeeId INT NOT NULL PRIMARY KEY,
-	FirstName VARCHAR(20),
-	LastName VARCHAR(20),
-	MiddleName VARCHAR(20) DEFAULT NULL,
-	PhoneNo INT NOT NULL,
-	FOREIGN KEY (OfficeId) REFERENCES Office(OfficeId)
-);
-
-
 CREATE TABLE Packages(
 	PackageId INT NOT NULL PRIMARY KEY,
 	FOREIGN KEY OfficeId REFERENCES Office(OfficeId) ,
@@ -112,6 +93,46 @@ CREATE TABLE Packages(
 	Type INT
 );
 
+--Office table
+--1. OfficeId
+--2. 
+CREATE TABLE Office(
+	OfficeId INT NOT NULL PRIMARY KEY,
+	AddressLine1  VARCHAR(20),
+	AddressLine2 VARCHAR(20),
+	AddressLine3 VARCHAR(20),
+	ZipCode INT
+);
+
+
+CREATE TABLE Employees(
+						EmployeeID INT NOT NULL PRIMARY KEY,
+						FirstName VARCHAR(20) NOT NULL,
+						LastName VARCHAR(20) NOT NULL,
+						MiddleName VARCHAR(20) DEFAULT NULL,
+						PhoneNo INT NOT NULL,
+						AddressLine1 VARCHAR(30),
+						AddressLine2 VARCHAR(30),
+						AddressLine3 VARCHAR(30),
+						ZipCode INT NOT NULL;
+						Email VARCHAR(30)
+);
+
+INSERT INTO Customers VALUES(1,"John", "Smith", "K", 2134190142, "House 12", "Privet Drive, Surrey", "London, England", "john@gmail.com");
+INSERT INTO Customers VALUES(2,"Joanne", "Smith", "A", 1234563891, "House 12", "Privet Drive, Surrey", "London, England", "joanne@gmail.com");
+INSERT INTO Customers VALUES(3,"Juan", "Smith", "K", 9234567890, "House 12", "Privet Drive, Surrey", "London, England", "juan@gmail.com");
+INSERT INTO Customers VALUES(4,"Jack", "Smith", "K", 8234567890, "House 12", "Privet Drive, Surrey", "London, England", "jackn@gmail.com");
+INSERT INTO Customers VALUES(5,"John", "Wick", "K", 7234567890, "House 12", "Privet Drive, Surrey", "London, England", "johnw@gmail.com");
+INSERT INTO Customers VALUES(6,"Harry", "Potter", "J", 6934567890, "House 12", "Privet Drive, Surrey", "London, England", "harryp@gmail.com");
+INSERT INTO Customers VALUES(7,"Frank", "Abagnale", "K", 4204567890, "House 12", "Privet Drive, Surrey", "London, England", "frank@gmail.com");
+INSERT INTO Customers VALUES(8,"Gandalf", "Grey", "The", 8904567890, "House 12", "Privet Drive, Surrey", "London, England", "gandalf@gmail.com");
+INSERT INTO Customers VALUES(9,"Aragorn", "Elessar", "", 2224567890, "House 12", "Privet Drive, Surrey", "London, England", "aragorn@gmail.com");
+INSERT INTO Customers VALUES(10,"Frodo", "Baggins", "", 2134567890, "House 12", "Privet Drive, Surrey", "London, England", "frodo@gmail.com");
+INSERT INTO Customers VALUES(11,"Ron", "Weasley", "K", 0114567890, "House 12", "Privet Drive, Surrey", "London, England", "weasley6@gmail.com");
+INSERT INTO Customers VALUES(12,"Fred", "Weasley", "K", 0124567890, "House 12", "Privet Drive, Surrey", "London, England", "fweasley@gmail.com");
+INSERT INTO Customers VALUES(13,"Saruman", "White", "The", 0864567890, "House 12", "Privet Drive, Surrey", "London, England", "saruman@gmail.com");
+INSERT INTO Customers VALUES(14,"Sahil", "Sulekhiya", "K", 0424567890, "House 12", "Privet Drive, Surrey", "London, England", "johns@gmail.com");
+INSERT INTO Customers VALUES(15,"Frank", "Castle", "K", 1934567890, "House 12", "Privet Drive, Surrey", "London, England", "smith@gmail.com");
 
 
 
@@ -133,15 +154,20 @@ INSERT INTO Packages VALUES(735099, 3, 839396, "sports", 3, 13);
 
 
 
-CREATE TABLE Employees(
-						EmployeeID INT NOT NULL PRIMARY KEY,
-						FirstName VARCHAR(20) NOT NULL,
-						LastName VARCHAR(20) NOT NULL,
-						MiddleName VARCHAR(20) DEFAULT NULL,
-						PhoneNo INT NOT NULL,
-						AddressLine1 VARCHAR(30),
-						AddressLine2 VARCHAR(30),
-						AddressLine3 VARCHAR(30),
-						ZipCode INT NOT NULL;
-						Email VARCHAR(30)
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
